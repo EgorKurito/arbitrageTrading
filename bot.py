df228ea88b3440a24dcc266f73995d772977fd83
Egor <ekurito@gmail.com>
2018-06-06 16:40:17 +0300

    doesnt work

--- bot.py
+++ bot.py
@@ -21,55 +21,26 @@ wss = BtfxWss()
 wss.start()
 
 while not wss.conn.connected.is_set():
-    time.sleep(1)
+    time.sleep(5)
 
 # Subscribe to some channels
-wss.subscribe_to_ticker('YYWETH')
-wss.subscribe_to_ticker('EOSETH')
-wss.subscribe_to_ticker('EOSUSD')
+wss.subscribe_to_ticker('OMGUSD')
+
 
 t = time.time()
-while time.time() - t < 0.5:
+while time.time() - t < 5:
     pass
 
-ticker_1 = wss.tickers('YYWETH')
-ticker_2 = wss.tickers('EOSETH')
-ticker_3 = wss.tickers('EOSUSD')
-'''
+ticker_1 = wss.tickers('OMGUSD')
+
+
 while True:
     ticker_11 = ticker_1.get()
     ticker_1_Ask = ticker_11[0][0][6]
     ticker_1_Bid = ticker_11[0][0][0]
 
-    ticker_22 = ticker_2.get()
-    ticker_2_Ask = ticker_22[0][0][6]
-    ticker_2_Bid = ticker_22[0][0][0]
-
-    ticker_33 = ticker_3.get()
-    ticker_3_Ask = ticker_33[0][0][6]
-    ticker_3_Bid = ticker_33[0][0][0]
-
-    x = 216.01542311
-    xxx = x * ticker_1_Ask / ticker_2_Ask * ticker_3_Bid
-    print(xxx)
-
-    time.sleep(7)
-'''
 
 
 
-# noinspection PyUnreachableCode
-class Bitfinex:
-    """
-    Algorithm of trade
-    """
-    def __init__(self, ticket1=None, ticket2=None, ticket3=None, ):
-        self.ticket1 = ticket1
-        self.ticket2 = ticket2
-        self.ticket3 = ticket3
-
-
-
-x
 
 
--- main.py
+++ main.py
@@ -1,3 +1,45 @@
 import requests
-url = 'https://api.bitfinex.com/v1/symbols'
-tickers = requests.get(url).text.upper()
+import logging
+import time
+import sys
+
+from btfxwss import BtfxWss
+
+# Make a 'list' of all pairs of crypto currency from the 'str' object
+tickets = requests.get('https://api.bitfinex.com/v1/symbols').text.upper().replace('"','')[1:-1].split(',')
+
+# Setting the BtfxWss Library
+log = logging.getLogger(__name__)
+
+fh = logging.FileHandler('test.log')
+fh.setLevel(logging.DEBUG)
+sh = logging.StreamHandler(sys.stdout)
+sh.setLevel(logging.DEBUG)
+
+log.addHandler(sh)
+log.addHandler(fh)
+logging.basicConfig(level=logging.DEBUG, handlers=[fh, sh])
+
+wss = BtfxWss()
+wss.start()
+
+while not wss.conn.connected.is_set():
+    time.sleep(1)
+
+for ticket in tickets:
+    wss.subscribe_to_ticker(ticket)
+
+    t = time.time()
+    while time.time() - t < 0.5:
+        pass
+
+    try:
+        ticker = wss.tickers(ticket).get()
+    except KeyError:
+        continue
+    # t_A - Ticker_Ask; t_B - Ticker_Bid
+    t_A = ticker[0][0][6]
+    t_B = ticker[0][0][0]
+
+    dif = ((t_A-t_B)/(t_A+t_B)/2)*100
+    print(dif)
