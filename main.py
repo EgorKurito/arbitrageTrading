5ac56744a3614be6745a4e4f9840649cf45716db
Egor <ekurito@gmail.com>
2018-06-27 20:50:44 +0300

    Test
    Test some pair

--- main.py
+++ main.py
@@ -9,24 +9,42 @@ from websocket import create_connection
 url = 'wss://api.bitfinex.com/ws/2'
 
 # Make a 'list' of all pairs of crypto currency from the 'str' object
-tickets = requests.get('https://api.bitfinex.com/v1/symbols').text.upper().replace('"','')[1:-1].split(',')
+#tickets = requests.get('https://api.bitfinex.com/v1/symbols').text.upper().replace('"','')[1:-1].split(',')
+tickets = ['EOSBTC', 'EOSETH', 'ETHBTC']
+# Good tickets
+good_tickets = []
 # Connection
 ws = create_connection(url, sslopt={"cert_reqs": ssl.CERT_NONE} )
 
-for ticket in tickets:
+def get_good_ticket(tickets):
+    for ticket in tickets:
 
-    ticker = dict(event="subscribe", channel="ticker", symbol="t{}".format(ticket))
-    ws.send(json.dumps(ticker))
+        ticker = dict(event="subscribe", channel="ticker", symbol="t{}".format(ticket))
+        ws.send(json.dumps(ticker))
 
-    while True:
-        result = json.loads(ws.recv())
-        if len(result) == 2:
-            # t_A - Ticker_Ask; t_B - Ticker_Bid
-            t_A = result[1][0]
-            t_B = result[1][2]
-            dif = ((t_A - t_B) / (t_A + t_B) / 2) * 100
-            if dif > -0.01:
-                print(ticket)
-                print(dif)
-            break
+        while True:
+            result = json.loads(ws.recv())
+            if (len(result) == 2) and (str(result)[-4:-2] != 'hb'):
+                # t_A - Ticker_Ask; t_B - Ticker_Bid
+                t_A = result[1][0]
+                t_B = result[1][2]
+                dif = ((t_A - t_B) / (t_A + t_B) / 2) * 100
 
+                t_ticket = [ticket, t_A, t_B, dif]
+                good_tickets.append(t_ticket)
+
+                break
+    return(good_tickets)
+
+
+
+def win():
+    ticket = get_good_ticket(tickets)
+    print(ticket)
+    x = 1
+    y = 1000
+    xx = x / ticket[0][1] * ticket[1][2] * ticket[2][1]
+    yy = y * ticket[0][2] / ticket[1][1]  / ticket[2][2]
+    return xx, yy
+
+print(win())
